03696b209d32224a630e13f6c71b64f37e5356a1
Kur0dono <yishak17@gmail.com>
2025-07-31 00:03:17 +0300

    Project ready for deployment

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,10 +10,13 @@ import userRoutes from "./routes/user.route.js";
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 
+import path  from "path";
+
 dotenv.config();
 //const app = express();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -27,6 +30,12 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/groups", groupRoutes)
 app.use("/api/users", userRoutes)
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log("Server is running on port PORT:" + PORT);
