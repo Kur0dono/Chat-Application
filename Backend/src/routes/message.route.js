16f607cb40d700a532772659b32245496c9b4386
Kur0dono <yishakmedhings@gmail.com>
2025-07-31 21:53:27 +0300

    Update message.route.js

--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -5,10 +5,11 @@ import { protectRoute } from '../middleware/auth.middleware.js';
 const router = express.Router();
 console.log("Loaded message routes");
 
-router.get("/:id", protectRoute, getMessages);
 router.get("/sidebar/last-messages", protectRoute, getLastMessagesForSidebar);
 router.post("/send/:id", protectRoute, sendMessage); 
 router.post("/send-group/:groupId", protectRoute, sendGroupMessage);
 router.get("/group/:groupId", protectRoute, getGroupMessages);
+
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
